a4a1685edc10b0cbf933a89c7c71afe0c64c26c9
Barretowski <phenriquevb@hotmail.com>
2023-05-16 20:16:16 -0300

    feat: navegando por links internos e histórico

--- app/src/main/java/com/example/mywebview/MainActivity.java
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.mywebview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +29,32 @@ public class MainActivity extends AppCompatActivity {
         settings.setJavaScriptEnabled(true);
         settings.setSupportZoom(false);
         webView.loadUrl("https://www.unoeste.br");
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if ("www.unoeste.br".equals(request.getUrl().getHost())) {
+                    // This is my website, so do not override; let my WebView load the page
+                    return false;
+                }
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                startActivity(intent);
+                return true;
+            }
+        });
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 
     private void toHome() {
